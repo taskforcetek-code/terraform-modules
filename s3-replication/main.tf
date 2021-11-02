ba3e1584c8f23bf8868c85453eea727aaaa6af0d
Kristopher Landon <landonkt@MBP16inch2021.localdomain>
2021-11-02 19:55:54 -0400

    fixing arn name

--- s3-replication/main.tf
+++ s3-replication/main.tf
@@ -140,7 +140,7 @@ resource "aws_s3_bucket" "backup" {
  server_side_encryption_configuration {
    rule {
      apply_server_side_encryption_by_default {
-       kms_master_key_id = aws_kms_key.bucket-key.arn
+       kms_master_key_id = aws_kms_key.bucket-key-replicated.arn
        sse_algorithm     = "aws:kms"
      }
    }
